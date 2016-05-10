05c096e8c38d59aa6965512db26a623d2c67b613
Chris-Vere <chris.vere@me.com>
2016-05-10 10:03:00 -0400

    Commit for rest of site

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Chris-Vere.github.io
+# Ally-Code-Challenge- Chris Vere
+
+Hello! Thank you for taking the time to review my challenge. Here are a couple notes regarding my work on the challenge.
+
+The page is resposive. I would normally add more breakpoints to the page, but with such a simple page design I kept it to a minimum.
+
+I wrote all my CSS in SASS and used CodeKit to concatenate and compile my .scss files. I would normally output minified CSS, but I left it uncompressed for easier review. The same goes for the JavaScript I wrote. I also included sourcemap files and I left in the sourcemap markers within the compiled CSS.
+
+I do know how to use jQuery quite well, I began using it about 6 years ago. However I chose to use raw JavaScript for this project. I have been using jQuery less frequently lately as I feel like the modern DOM API can do many of the things jQuery can, but without having to load an entire library.
+
+I used no plugins on this project, but I did conditionally load a couple polyfills to ensure IE9 could use things like element.classList amd element.dataset. jQuery would have silimilar 'polyfills' built into its core, so I felt no-harm done.
+
+I am aware the off-canvas navigation for small screens does not work in IE9, but I feel that should be OK since no devices are running IE9.
+
+I tested the site in the following browsers:
+
+- Chrome- Latest version on Mac, PC
+- FireFox- Latest version on Mac, PC
+- Safari- Mac, iPhone 5, iPad Air, last iOS
+- IE9
+
+Unfortunately, I do not have access at home to Android devices or newer versions or IE.
+
+Thanks again,
+Chris Vere
---
+++ js/script.js
@@ -0,0 +1,226 @@
+function log(msg){
+	console.log( msg );
+}
+
+function newElem(name){
+	return document.createElement(name);
+}
+
+// mobile nav toggle
+(function(){
+	var html    = document.querySelector('html'),
+		menuBtn = document.querySelector('.mobile-nav-toggle'),
+		navIsShowing = false,
+		classnameShowNav = 'showNav';
+
+	function toggleMobileNav(){
+		if( navIsShowing ){
+			html.classList.remove( classnameShowNav );
+		}else{
+			html.classList.add( classnameShowNav );
+		}
+
+		navIsShowing = !navIsShowing;
+	}
+
+	menuBtn.addEventListener('click', toggleMobileNav);
+
+})();
+
+// tabs
+(function(){
+	var tabs = document.querySelectorAll('.tabs'),
+		classNameActiveTab = 'tabs__item--is-active',
+		classNameActivePanel = 'tabs__panel--is-active';
+
+	for(var i = 0; i < tabs.length; i++){
+		var tabInstance = tabs[i],
+			tabLinks = tabInstance.querySelectorAll('.tabs__link'),
+			activeLink,
+			activePanel;
+
+
+		function handleTabClicked(e){
+			var item = this,
+				targetPanel = tabInstance.querySelector(item.getAttribute( 'href' ));
+
+			activeLink.setAttribute('aria-selected', false);
+			activeLink.parentNode.classList.remove( classNameActiveTab );
+			activeLink = item;
+			activeLink.setAttribute('aria-selected', true);
+			activeLink.parentNode.classList.add( classNameActiveTab );
+
+			activePanel.classList.remove( classNameActivePanel );
+			activePanel.setAttribute('aria-hidden', 'true');
+			activePanel = targetPanel;
+			activePanel.setAttribute('aria-hidden', 'false');
+			activePanel.classList.add( classNameActivePanel );
+
+			e.preventDefault();
+			return false;
+		}
+
+		for(var j = 0; j < tabLinks.length; j++){
+			var tabItem = tabLinks[j];
+			if( tabItem.parentNode.classList.contains( classNameActiveTab ) ){
+				activeLink  = tabItem;
+				activePanel = tabInstance.querySelector(activeLink.getAttribute('href'));
+			}
+			tabItem.addEventListener('click', handleTabClicked);
+		}
+
+	}
+})();
+
+// login modal
+(function(){
+	var html    = document.querySelector('html'),
+		overlay = document.querySelector('.overlay'),
+		modals  = overlay.querySelectorAll('.overlay-module'),
+		classNameOverlayIsActive = "overlay--is-active",
+		classNameOverlayIsBlock  = "overlay--is-block",
+		classNameModalOpen = 'modal-open',
+		currentModal;
+
+	var modalLaunchers = document.querySelectorAll('[data-modal-launch]');
+
+	function handleLaunchClicked(e){
+		var targetModalId = this.dataset.modalTarget;
+		var targetModal   = getModal( targetModalId );
+
+		currentModal = targetModal;
+
+		positionOverlay();
+		launchOverlay();
+		focusFirstField();
+
+		html.addEventListener('keydown', onKeyDown);
+
+		e.preventDefault();
+		return false;
+	}
+
+	function getModal(modalId){
+		for(var i = 0; i < modals.length; i++){
+			// would use === for comparison here, but IE9 doesn't like it :(
+			if( modals[i].getAttribute('id') == modalId ){
+				return modals[i];
+			}
+		}
+	}
+
+	function onKeyDown(e){
+		if(e.keyCode == 27){
+			closeCurrentModal();
+		}
+	}
+
+	function positionOverlay(){
+		var body = document.body,
+		    html = document.documentElement;
+
+		var height = Math.max(
+				body.scrollHeight,
+				body.offsetHeight,
+				html.clientHeight,
+				html.scrollHeight,
+				html.offsetHeight
+			);
+
+		overlay.style.height = height + "px";
+		currentModal.style.top = window.scrollY + 'px';
+	}
+
+	function launchOverlay(){
+		overlay.classList.add(classNameOverlayIsBlock);
+		overlay.getBoundingClientRect();
+		overlay.classList.add(classNameOverlayIsActive);
+	}
+
+	function closeCurrentModal(){
+		overlay.classList.remove( classNameOverlayIsActive );
+		html.removeEventListener('keydown', onKeyDown);
+		window.setTimeout(function(){
+			overlay.classList.remove(classNameOverlayIsBlock);
+		}, 200);
+	}
+
+	function focusFirstField(){
+		var inputs = currentModal.querySelectorAll('input');
+		inputs[0].focus();
+	}
+
+	for(var i = 0; i < modalLaunchers.length; i++){
+		modalLaunchers[i].addEventListener('click', handleLaunchClicked);
+	}
+
+	for(var i = 0; i < modals.length; i++){
+		var closeBtns = modals[i].querySelectorAll('[data-modal-close]');
+		for(var j = 0; j < closeBtns.length; j++){
+			closeBtns[i].addEventListener('click', closeCurrentModal);
+		}
+	}
+
+})();
+
+// data table
+(function(){
+	var table = document.querySelector( '.table--feature' );
+	var tbody = newElem('tbody');
+	var respAttrs = [ "", "Annual Percentage Yield", "Est. Earnings for 1 Year*" ];
+
+	function buildTable(data){
+		for(var i = 0; i < data.length; i++){
+			var row = newElem('tr');
+
+			var index = 0;
+			for( val in data[i] ){
+				var td = newElem('td');
+				td.dataset.respText = respAttrs[index];
+				td.textContent = data[i][val];
+				row.appendChild( td );
+				index++;
+			}
+			tbody.appendChild(row);
+		}
+		table.appendChild( tbody );
+	}
+
+	function buildError(){
+		var row = newElem('tr');
+		var cell = newElem('td');
+		cell.classList.add('center')
+		cell.setAttribute('colspan', '3');
+		cell.textContent = 'Sorry, there was an error loading the content.';
+
+		row.appendChild( cell );
+		tbody.appendChild( row );
+		table.appendChild( tbody );
+	}
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'data/code-test.json');
+	xhr.send(null);
+
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200){
+				buildTable(JSON.parse(xhr.responseText));
+			}else{
+				buildError();
+			}
+		}
+	}
+})();
+
+
+
+
+
+
+
+
+
+
+
+
